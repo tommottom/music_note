6c6e75fbf5b24b4c74019c7eb255d55ce31a68e9
rudiments <fb53ice4j@gmail.com>
2015-09-15 09:13:44 +0000

    finsih

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -1,6 +1,10 @@
 class PinsController < ApplicationController
     before_action :find_pin,  only: [:show, :edit, :update, :destroy, :upvote]
-   
+    before_action :authenticate_user!, except: [:index, :show]
+    
+    def about
+    end
+    
     def index
         @pins = Pin.all.order("created_at DESC")
     end
